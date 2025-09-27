faa3f39131ca3f49d99659cf507cac0cc396fc68
Shahid Aktar Mir <110034914+ShahidAktarMir@users.noreply.github.com>
2025-09-27 10:58:19 +0530

    Fix duplicate import and syntax errors in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 import App from './App';
 import { store } from '@/store';
-import { store } from './store';
+import ErrorBoundary from '@/components/common/ErrorBoundary';
 import '@/styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -14,8 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Provider store={store}>
         <BrowserRouter>
           <App />
-          <Toaster position="top-right" />
-        </BrowserRouter>
+          <Toaster 
+            position="top-right"
+            toastOptions={{
               style: {
                 background: '#363636',
                 color: '#fff',
